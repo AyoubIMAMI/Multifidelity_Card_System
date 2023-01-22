39ced4524ccb8e6ca7abfd4619a909c4476f2db6
KilianBonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-01-22 14:38:06 +0100

    Exceptions

--- src/main/java/fr/polytech/pojo/BankTransaction.java
+++ src/main/java/fr/polytech/pojo/BankTransaction.java
@@ -3,6 +3,8 @@ package fr.polytech.pojo;
 public class BankTransaction {
     private Customer customer;
 
+    private float amount;
+
     private String cardNumber;
     private String cardHolderName;
     private String cvv;
