57b59b7a3b9eedadfe01f4ec36f4821615bd33d8
MouradKarrakchou <mourad.karrakchou@gmail.com>
2023-01-22 16:37:08 +0100

    Interface update

--- src/main/java/fr/polytech/interfaces/store/StoreExplorer.java
+++ src/main/java/fr/polytech/interfaces/store/StoreExplorer.java
@@ -1,8 +1,7 @@
 package fr.polytech.interfaces.store;
 
-import fr.polytech.exceptions.BadCredentialsException;
 import fr.polytech.exceptions.store.StoreNotFoundException;
 
 public interface StoreExplorer {
-    void findStore(String name, String myName, String myPassword)  throws BadCredentialsException, StoreNotFoundException;
+    void getOpeningHours(String name)  throws StoreNotFoundException;
 }
