cdbbea643f326f5bf136231bfcf85a0e55f25c6a
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2023-01-22 15:21:03 +0100

    add a lot of thing

--- src/main/java/fr/polytech/interfaces/advantage/AdvantageConsumer.java
+++ src/main/java/fr/polytech/interfaces/advantage/AdvantageConsumer.java
@@ -1,7 +1,8 @@
 package fr.polytech.interfaces.advantage;
 
+import fr.polytech.exceptions.advantage.NoAdvantageFoundException;
 import fr.polytech.pojo.Advantage;
 
 public interface AdvantageConsumer {
-    void consumeAdvantage(Advantage advantage);
+    void consumeAdvantage(Advantage advantage) throws NoAdvantageFoundException;
 }
--- src/main/java/fr/polytech/interfaces/advantage/VFPTransaction.java
+++ src/main/java/fr/polytech/interfaces/advantage/VFPTransaction.java
@@ -1,7 +1,10 @@
 package fr.polytech.interfaces.advantage;
 
+import fr.polytech.exceptions.CustomerNotFoundException;
+import fr.polytech.exceptions.NotVFPClientException;
+import fr.polytech.exceptions.advantage.ISawWhereYouParkedLastSummerUnvailableException;
 import fr.polytech.pojo.Advantage;
 
 public interface VFPTransaction {
-    boolean tryUseAdvantage(String username, Advantage advantage);
+    boolean tryUseAdvantage(String username, Advantage advantage) throws CustomerNotFoundException, NotVFPClientException, ISawWhereYouParkedLastSummerUnvailableException;
 }
