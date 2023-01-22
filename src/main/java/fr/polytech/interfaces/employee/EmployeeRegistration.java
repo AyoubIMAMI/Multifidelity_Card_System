55a1adcc6a767d1afb8d5efc8020a3d0f934a3f2
KilianBonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-01-22 16:17:31 +0100

    EmployeeRegistration

--- src/main/java/fr/polytech/interfaces/employee/EmployeeRegistration.java
+++ src/main/java/fr/polytech/interfaces/employee/EmployeeRegistration.java
@@ -1,11 +1,19 @@
 package fr.polytech.interfaces.employee;
 
 import fr.polytech.exceptions.NotEnoughPermissionException;
-import fr.polytech.exceptions.WrongEmployeeNameOrPassword;
 import fr.polytech.exceptions.store.EmployeeNotFoundException;
 import fr.polytech.pojo.structure.Employee;
 import fr.polytech.pojo.structure.Role;
 
 public interface EmployeeRegistration {
-    void addEmployee(Employee employee, String newEmployeeName, String newEmployeePassword, Role newEmployeeRole) throws NotEnoughPermissionException, EmployeeNotFoundException, WrongEmployeeNameOrPassword;
+    /**
+     * Add an employee to the registry
+     * @param employee The employee in charge of adding a new employee
+     * @param newEmployeeName The name of the new employee
+     * @param newEmployeePassword The password of the new employee
+     * @param newEmployeeRole The role of the new employee
+     * @throws NotEnoughPermissionException The employee in charge of adding a new employee doesn't have the required permissions
+     * @throws EmployeeNotFoundException The employee in charge of adding a new employee wasn't found
+     */
+    void addEmployee(Employee employee, String newEmployeeName, String newEmployeePassword, Role newEmployeeRole) throws NotEnoughPermissionException, EmployeeNotFoundException;
 }
