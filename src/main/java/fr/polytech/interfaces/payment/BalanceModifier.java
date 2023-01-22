bd8506fb58d442464dd614b3b37606f1902e954e
KilianBonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-01-22 15:00:27 +0100

    Customer

--- src/main/java/fr/polytech/interfaces/payment/BalanceModifier.java
+++ src/main/java/fr/polytech/interfaces/payment/BalanceModifier.java
@@ -3,9 +3,10 @@ package fr.polytech.interfaces.payment;
 import fr.polytech.exceptions.MalformedBankInformationException;
 import fr.polytech.exceptions.NotEnoughBalanceException;
 import fr.polytech.pojo.BankTransaction;
+import fr.polytech.pojo.Customer;
 import fr.polytech.pojo.FidelityAccount;
 
 public interface BalanceModifier {
-    void decreaseBalance(FidelityAccount fidelityAccount, float amount) throws NotEnoughBalanceException;
-    void rechargeBalance(FidelityAccount fidelityAccount, BankTransaction bankTransaction, float amount) throws MalformedBankInformationException;
+    void decreaseBalance(Customer customer, float amount) throws NotEnoughBalanceException;
+    void rechargeBalance(Customer customer, BankTransaction bankTransaction, float amount) throws MalformedBankInformationException;
 }
