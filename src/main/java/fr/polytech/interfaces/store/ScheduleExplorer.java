5facdf6ab95474dc4064a481ce68a2d56ea9446d
AyoubIMAMI <youbima17@gmail.com>
2023-01-22 16:12:48 +0100

    Schedule

--- src/main/java/fr/polytech/interfaces/store/ScheduleExplorer.java
+++ src/main/java/fr/polytech/interfaces/store/ScheduleExplorer.java
@@ -1,10 +1,11 @@
 package fr.polytech.interfaces.store;
 
 import fr.polytech.exceptions.StoreNotFoundException;
+import fr.polytech.exceptions.store.NotValidDayException;
 import fr.polytech.pojo.structure.Store;
 
 public interface ScheduleExplorer {
     boolean isStoreOpen(Store store) throws StoreNotFoundException;
-
-
+    String getOpeningHour(Store store, String day) throws StoreNotFoundException, NotValidDayException;
+    String getClosingHour(Store store, String day) throws StoreNotFoundException, NotValidDayException;
 }
