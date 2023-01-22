d9de224d53d57a793ba201f76ba1366d602b9965
AyoubIMAMI <youbima17@gmail.com>
2023-01-22 14:03:31 +0100

    CustomerAccount Interfaces

--- src/main/java/fr/polytech/interfaces/Customer/CustomerRegistration.java
+++ src/main/java/fr/polytech/interfaces/Customer/CustomerRegistration.java
@@ -3,8 +3,7 @@ package fr.polytech.interfaces.Customer;
 import fr.polytech.pojo.Customer;
 
 public interface CustomerRegistration {
-    void register(Customer customer);
-    //This method below might be included in the register() method
-    boolean checkRegistryInformation(Customer customer);
-    boolean login(Customer customer);
+    Customer register(String name, String mail, String password);
+    //return the token as a String
+    String login(String mail, String password);
 }
