cdc4ca574e35b36c937ff8d3af09478ac9002a77
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2023-01-23 12:50:35 +0100

    add licencePlate

--- src/main/java/fr/polytech/pojo/FidelityAccount.java
+++ src/main/java/fr/polytech/pojo/FidelityAccount.java
@@ -1,5 +1,6 @@
 package fr.polytech.pojo;
 
+import java.util.Optional;
 import java.util.Set;
 
 public class FidelityAccount {
@@ -8,4 +9,5 @@ public class FidelityAccount {
     private float balance;
     private boolean isVFP;
     private Set<Advantage> advantages;
+    private String licencePlate;
 }
