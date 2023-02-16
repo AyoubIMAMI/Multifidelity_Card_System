e1e48975918a5cd81ab7afa8d4c3d04de17e3265
MouradKarrakchou <mourad.karrakchou@gmail.com>
2023-02-16 10:03:36 +0100

    setup test #2

--- src/main/java/fr/polytech/components/customer/CustomerManager.java
+++ src/main/java/fr/polytech/components/customer/CustomerManager.java
@@ -1,19 +1,23 @@
 package fr.polytech.components.customer;
 
-import fr.polytech.repository.CustomerRepository;
+import fr.polytech.exceptions.BadCredentialsException;
 import fr.polytech.exceptions.CustomerNotFoundException;
 import fr.polytech.exceptions.MailAlreadyUsedException;
+import fr.polytech.exceptions.MalformedCredentialsExceptions;
+import fr.polytech.interfaces.customer.CustomerExplorer;
 import fr.polytech.interfaces.customer.CustomerFinder;
 import fr.polytech.interfaces.customer.CustomerRegistration;
 import fr.polytech.pojo.Customer;
+import fr.polytech.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.StreamSupport;
 
 @Component
-public class CustomerManager implements CustomerRegistration, CustomerFinder {
+public class CustomerManager implements CustomerRegistration, CustomerFinder, CustomerExplorer {
 
     CustomerRepository customerRepository;
 
@@ -38,5 +42,13 @@ public class CustomerManager implements CustomerRegistration, CustomerFinder {
         Optional<Customer> customer= customerRepository.findById(id);
         if (customer.isEmpty()) throw new CustomerNotFoundException();
         else return customer.get();
+    }
+
+    @Override
+    public UUID checkCredentials(String email, String password) throws BadCredentialsException {
+        Optional<Customer> customerCurrent=StreamSupport.stream(customerRepository.findAll().spliterator(), false)
+                .filter(customer -> email.equals(customer.getEmail())&&password.equals(customer.getPassword())).findAny();
+        if (customerCurrent.isEmpty()) throw new BadCredentialsException();
+        else return customerCurrent.get().getId();
     }
 }
