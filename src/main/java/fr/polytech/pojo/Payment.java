dc5a0d052288f7c6feef038f6e0edf856c6a2492
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2023-02-16 10:10:34 +0100

    fix Payement, Client -> Customer #7

--- src/main/java/fr/polytech/pojo/Payment.java
+++ src/main/java/fr/polytech/pojo/Payment.java
@@ -1,6 +1,7 @@
 package fr.polytech.pojo;
 
 import ch.qos.logback.core.net.server.Client;
+import fr.polytech.components.customer.CustomerFidelityManager;
 import fr.polytech.pojo.item.Product;
 import fr.polytech.pojo.structure.Employee;
 import fr.polytech.pojo.structure.Store;
@@ -9,11 +10,13 @@ import java.util.Set;
 
 public class Payment {
     private int id;
-    private Client client;
+    private Customer customer;
     private Store store;
     private Employee employee;
-
     private Set<Product> shoppingList;
-
     private boolean isSettled;
+
+    public boolean isSettled() {
+        return isSettled;
+    }
 }
