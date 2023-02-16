f5288a2de9ad950ecd33da18731b037d34725ba6
AyoubIMAMI <youbima17@gmail.com>
2023-02-16 09:54:57 +0100

    registration #1

--- src/main/java/fr/polytech/components/customer/CustomerManager.java
+++ src/main/java/fr/polytech/components/customer/CustomerManager.java
@@ -24,12 +24,13 @@ public class CustomerManager implements CustomerRegistration, CustomerFinder {
 
     @Override
     public Customer register(String name, String mail, String password) throws MailAlreadyUsedException {
-        try{
+        if(customerRepository.isMailAlreadyUsed(mail))
+            throw new MailAlreadyUsedException();
+
+        else {
             Customer customer = new Customer(name, mail, password);
             customerRepository.save(customer, customer.getId());
             return customer;
-        } catch (Exception exception) {
-            throw new MailAlreadyUsedException();
         }
     }
 
