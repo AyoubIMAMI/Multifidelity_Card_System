0ae78ca49bd45e13fb9ad5a0296a61d19b6dbafa
AyoubIMAMI <youbima17@gmail.com>
2023-02-16 08:41:02 +0100

    CustomerManager #1

--- src/main/java/fr/polytech/components/CustomerManager.java
+++ src/main/java/fr/polytech/components/CustomerManager.java
@@ -1,5 +1,14 @@
 package fr.polytech.components;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerManager {
+
+    @Autowired
+    CustomerManager(){
+
+    }
 
 }
