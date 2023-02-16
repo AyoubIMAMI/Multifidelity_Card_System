f9b1a7375686347c5da7268898f7d0e84a89440e
Antoine Buquet <antoine.buquet49@gmail.com>
2023-02-16 09:43:05 +0100

    Finish PointPurchaseHandler #4

--- src/main/java/fr/polytech/pojo/Payment.java
+++ src/main/java/fr/polytech/pojo/Payment.java
@@ -1,6 +1,7 @@
 package fr.polytech.pojo;
 
 import ch.qos.logback.core.net.server.Client;
+import fr.polytech.pojo.item.Item;
 import fr.polytech.pojo.item.Product;
 import fr.polytech.pojo.structure.Employee;
 import fr.polytech.pojo.structure.Store;
@@ -13,7 +14,25 @@ public class Payment {
     private Store store;
     private Employee employee;
 
-    private Set<Product> shoppingList;
+    private Set<Item> shoppingList;
 
     private boolean isSettled;
+
+    private float price;
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Set<Item> getShoppingList() {
+        return shoppingList;
+    }
+
+    public void setShoppingList(Set<Item> shoppingList) {
+        this.shoppingList = shoppingList;
+    }
 }
