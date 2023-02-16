276bf3c9fd8e4804649fd2e00d13349b87a170f8
Antoine Buquet <antoine.buquet49@gmail.com>
2023-02-16 10:57:36 +0100

    start test PointPurchaseHandler #4

--- backend/src/test/java/fr/polytech/components/payment/PointPurchaseHandlerTest.java
+++ backend/src/test/java/fr/polytech/components/payment/PointPurchaseHandlerTest.java
@@ -1,5 +1,7 @@
 package fr.polytech.components.payment;
 
+import fr.polytech.unit.Customer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,5 +9,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class PointPurchaseHandlerTest {
 
+    Customer customer;
+
+    @BeforeEach
+    void setUp() {
+        customer = new Customer();
+    }
+
+    @Test
+    void givenCustomerWithPointsOnFidelityAccount_whenBuyingWithPoint_thenPointsAreModified() {
+        // Given
+
+
+    }
 
 }
