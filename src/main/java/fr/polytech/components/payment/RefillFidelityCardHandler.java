8373c5a2e1bf7e0ef0037c26c8e8902ce128c527
KilianBonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-02-16 09:39:35 +0100

    Adding bank call #6

--- src/main/java/fr/polytech/components/payment/RefillFidelityCardHandler.java
+++ src/main/java/fr/polytech/components/payment/RefillFidelityCardHandler.java
@@ -1,15 +1,26 @@
 package fr.polytech.components.payment;
 
 import fr.polytech.exceptions.MalformedBankInformationException;
+import fr.polytech.exceptions.paiment.PaymentException;
+import fr.polytech.interfaces.payment.Bank;
 import fr.polytech.interfaces.payment.RefillFidelityCard;
 import fr.polytech.pojo.BankTransaction;
 import fr.polytech.pojo.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
+@Component
 public class RefillFidelityCardHandler implements RefillFidelityCard {
 
+    @Autowired
+    private Bank bank;
 
     @Override
-    public void refill(Customer customer, BankTransaction transaction, float amount) throws MalformedBankInformationException {
-
+    public Date refill(Customer customer, BankTransaction transaction) throws MalformedBankInformationException, PaymentException {
+        if(bank.pay(customer, transaction))
+            return new Date();
+        throw new PaymentException();
     }
 }
