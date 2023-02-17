595414ccdaaa41ede6a681e2939a513a2362624a
KilianBonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-02-17 09:57:30 +0100

    Updating PaymentDTO

--- backend/src/main/java/fr/polytech/components/payment/RefillFidelityCardHandler.java
+++ backend/src/main/java/fr/polytech/components/payment/RefillFidelityCardHandler.java
@@ -1,14 +1,16 @@
 package fr.polytech.components.payment;
 
-import fr.polytech.exceptions.MalformedBankInformationException;
+import fr.polytech.exceptions.NegativeAmountException;
 import fr.polytech.exceptions.paiment.PaymentException;
 import fr.polytech.interfaces.payment.Bank;
 import fr.polytech.interfaces.payment.RefillFidelityCard;
-import fr.polytech.pojo.BankTransaction;
+import fr.polytech.pojo.Customer;
+import fr.polytech.pojo.PaymentDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Component
 public class RefillFidelityCardHandler implements RefillFidelityCard {
@@ -17,9 +19,9 @@ public class RefillFidelityCardHandler implements RefillFidelityCard {
     private Bank bank;
 
     @Override
-    public Date refill(BankTransaction transaction) throws MalformedBankInformationException, PaymentException {
+    public Date refill(Customer customer, PaymentDTO transaction) throws NegativeAmountException, PaymentException {
         if(transaction.getAmount() <= 0)
-            throw new MalformedBankInformationException();
+            throw new NegativeAmountException();
 
         if(bank.pay(transaction))
             return new Date();
---
+++ backend/src/main/java/fr/polytech/exceptions/paiment/NegativeAmountException.java
@@ -0,0 +1,4 @@
+package fr.polytech.exceptions.paiment;
+
+public class NegativeAmountException extends Exception {
+}
