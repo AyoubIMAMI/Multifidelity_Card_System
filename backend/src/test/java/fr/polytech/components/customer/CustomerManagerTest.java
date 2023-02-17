553f57475dcfce60aa24f411e8dc0bea2ae76c9d
AyoubIMAMI <youbima17@gmail.com>
2023-02-17 09:53:52 +0100

    registration test #1

--- backend/src/test/java/fr/polytech/components/customer/CustomerManagerTest.java
+++ backend/src/test/java/fr/polytech/components/customer/CustomerManagerTest.java
@@ -3,6 +3,7 @@ package fr.polytech.components.customer;
 import fr.polytech.exceptions.MailAlreadyUsedException;
 import fr.polytech.interfaces.customer.CustomerRegistration;
 import fr.polytech.pojo.Customer;
+import fr.polytech.repository.CustomerRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +16,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class CustomerManagerTest {
 
     @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
     private CustomerRegistration customerRegistration;
 
     private String name;
     private String mail;
     private String password;
+
 
     @BeforeEach
     void setUp() {
@@ -29,13 +34,16 @@ public class CustomerManagerTest {
     }
 
     @Test
-    void registration() throws MailAlreadyUsedException {
+    void registrationTest() throws MailAlreadyUsedException {
         Customer customer = customerRegistration.register(name, mail, password);
+
         assertEquals(name, customer.getName());
         assertEquals(mail, customer.getEmail());
         assertEquals(password, customer.getPassword());
 
-        //assertTrue(customerRegistration.);
+        assertTrue(customerRepository.existsById(customer.getId()));
+        assertEquals(1, customerRepository.count());
+        assertEquals(customer, customerRepository.findById(customer.getId()).get());
     }
 
 }
