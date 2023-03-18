2b48d89fbcbd928cc887d94c6037acec68bbd1a3
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-03-18 10:28:05 +0100

    setup Dockers

---
+++ cli/src/main/java/fr/univcotedazur/simpletcfs/cli/model/CliCustomer.java
@@ -0,0 +1,57 @@
+package fr.univcotedazur.simpletcfs.cli.model;
+import java.util.Objects;
+
+
+public class CliCustomer {
+
+    private Long id;
+
+    private String name;
+    private String password;
+    private String email;
+    private CliFidelityAccount fidelityAccount;
+
+    //@OneToMany(cascade = {CascadeType.REMOVE}, fetch = FetchType.LAZY, mappedBy = "customer")
+    //private List<Payment> history;
+
+    public CliCustomer(String name, String email, String password){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.fidelityAccount = new CliFidelityAccount(id);
+        //this.history = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public CliFidelityAccount getFidelityAccount() {
+        return fidelityAccount;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CliCustomer customer = (CliCustomer) o;
+
+        if (!Objects.equals(name, customer.name)) return false;
+        if (!Objects.equals(password, customer.password)) return false;
+        if (!Objects.equals(email, customer.email)) return false;
+        return Objects.equals(fidelityAccount, customer.fidelityAccount);
+    }
+}
+
