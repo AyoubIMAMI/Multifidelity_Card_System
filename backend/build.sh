f9bdf74ace43bc60321a49555b8d2ba22568a090
Antoine Buquet <61704871+antoinebqt@users.noreply.github.com>
2023-04-01 17:01:29 +0200

    build sh

--- backend/build.sh
+++ backend/build.sh
@@ -3,4 +3,3 @@
 echo "Compiling the TCF Spring BACKEND within a multi-stage docker build"
 
 docker build --build-arg JAR_FILE=isa-devops-22-23-team-h-23-1.1-SNAPSHOT.jar -t nget/spring-backend .
-read -p "Press to close ..."
