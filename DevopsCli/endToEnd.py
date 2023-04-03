aef65f98d866857ae60945ed1f2273ef0852b3ea
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-04-03 22:47:19 +0200

    create py

---
+++ DevopsCli/endToEnd.py
@@ -0,0 +1,64 @@
+import psycopg2
+import os
+import time
+#apt-get install -y socat
+#sudo apt install python3-pip
+#pip install psycopg2-binary
+
+
+
+def getTable(tableName):
+    # Exécuter une requête SELECT pour obtenir le contenu de la table 'customer'
+    cursor.execute("SELECT * FROM " + tableName)
+
+    # Parcourir les résultats de la requête et les afficher
+    userTable = cursor.fetchall()
+    return userTable
+
+def printTable(table):
+    print("print ",table[0])
+    for row in table:
+        print(row)
+
+def connect():
+    global connection
+    global cursor
+    # Définir les informations de connexion à la base de données
+    connection = psycopg2.connect(
+        host="localhost",
+        database="tcf-db",
+        user="postgresuser",
+        password="postgrespass",
+        port=5432
+    )
+    return connection
+
+def disconnect(cursor, connection):
+    cursor.close()
+    connection.close()
+
+
+
+# Ouvrir une connexion et un curseur
+print("Hello World")
+connection = connect()
+cursor = connection.cursor()
+
+userTable = getTable("customer")
+printTable(userTable)
+
+organisationTable = getTable("organisation")
+printTable(organisationTable)
+
+os.system('./test.sh')
+time.sleep(3)
+
+userTable = getTable("customer")
+printTable(userTable)
+
+organisationTable = getTable("organisation")
+printTable(organisationTable)
+
+# Fermer le curseur et la connexion à la base de données
+cursor.close()
+connection.close()
