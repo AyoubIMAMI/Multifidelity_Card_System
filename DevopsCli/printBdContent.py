7d0aa578e57a6a353d3b250f649c75311627520c
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-04-03 23:29:50 +0200

    update db port

---
+++ DevopsCli/printBdContent.py
@@ -0,0 +1,34 @@
+import psycopg2
+
+# Définir les informations de connexion à la base de données
+conn = psycopg2.connect(
+    host="localhost",
+    database="tcf-db",
+    user="postgresuser",
+    password="postgrespass",
+    port=5432
+)
+
+# Ouvrir un curseur
+cur = conn.cursor()
+
+# Obtenir les noms de toutes les tables de la base de données
+cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
+
+# Parcourir les résultats de la requête et afficher le contenu de chaque table
+for table in cur.fetchall():
+    # Afficher le nom de la table
+    print(f"Contenu de la table '{table[0]}':")
+    
+    # Exécuter une requête SELECT pour obtenir le contenu de la table
+    cur.execute(f"SELECT * FROM {table[0]}")
+    
+    # Parcourir les résultats de la requête et les afficher
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    print("") # Laisser une ligne vide entre chaque table
+
+# Fermer le curseur et la connexion à la base de données
+cur.close()
+conn.close()
--- backend/build.sh
+++ backend/build.sh
@@ -5,5 +5,8 @@ echo "Maven Clean Package"
 mvn clean package
 
 echo "Compiling the Spring BACKEND within a multi-stage docker build"
+export MY_JAR_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
-docker build --build-arg JAR_FILE=isa-devops-22-23-team-h-23-1.0-SNAPSHOT.jar -t teamh-backend .
+docker build --build-arg JAR_FILE=isa-devops-22-23-team-h-23-$MY_JAR_VERSION.jar -t teamh-backend .
+
+
