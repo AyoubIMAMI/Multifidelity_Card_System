fd75c29236b490a65c7404d696c9830dab70e24d
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-04-05 11:40:06 +0200

    add more tests in payment

--- backend/src/test/java/fr/polytech/cucumber/payment/PaymentDef.java
+++ backend/src/test/java/fr/polytech/cucumber/payment/PaymentDef.java
@@ -37,11 +37,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class PaymentDef {
+    Exception catchedExeption;
     Store store;
     Customer customer;
 
@@ -79,6 +81,7 @@ public class PaymentDef {
         String password = "myPassword";
         customerRepository.save(new Customer(name,mail,password));
         customer=customerRepository.findCustomerByName(name).get();
+        shoppingList=new HashSet<>();
     }
 
     @And("a store")
@@ -91,13 +94,6 @@ public class PaymentDef {
         store=storeRepository.findStoreByName(name).get();
     }
 
-    @When("he want to buy items")
-    public void heWantToBuyItems() {
-        shoppingList=new HashSet<>();
-        shoppingList.add(new Item(2,new Product("Coffee", store.getId(), 5)));
-        shoppingList.add(new Item(2,new Discount("Cake", store.getId(),  10, 7)));
-    }
-
     @And("he has enough cash")
     public void heHasEnoughCash() throws NegativeAmountException, PaymentInBankException {
         refillFidelityCard.refill(customer,new BankTransactionDTO("896983", 100));
@@ -106,7 +102,13 @@ public class PaymentDef {
     @And("he pay")
     public void hePay() throws PaymentAlreadyExistsException, NoDiscountsFoundException, BadCredentialsException, CustomerNotFoundException, NotEnoughBalanceException, PurchaseFailedException, StoreNotFoundException {
         pointModifier.incrementPoints(customer, 1000);
-        payment.payWithFidelity(customer.getId(),store.getId(),shoppingList);
+
+        try{
+            payment.payWithFidelity(customer.getId(),store.getId(),shoppingList);
+
+        }catch(NotEnoughBalanceException e){
+            catchedExeption = e;
+        }
     }
 
     @Then("the payment works")
@@ -114,5 +116,28 @@ public class PaymentDef {
         ArrayList<Payment> payments=new ArrayList<Payment>();
         paymentRepository.findAllByCustomer(customer).forEach(pay->payments.add(pay));
         assertEquals(1,payments.size());
+    }
+
+    @When("he want to buy an item names {string}, it cost {int}, in quantity {int}")
+    public void heWantToBuyAnItemNamesItCostInQuantity(String name, int price, int quantity) {
+        shoppingList.add(new Item(quantity,new Product(name, store.getId(), price)));
+    }
+
+    @When("he want to buy an discount names {string}, it cost {int} and {int} points, in quantity {int}")
+    public void heWantToBuyAnDiscountNamesItCostInQuantity(String name, int price, int points, int quantity) {
+        shoppingList.add(new Item(quantity,new Discount(name, store.getId(),  price, points)));
+    }
+
+    @And("he has not enough cash")
+    public void heHasNotEnoughCash() throws NegativeAmountException, PaymentInBankException {
+        refillFidelityCard.refill(customer,new BankTransactionDTO("896983", 1));
+    }
+
+    @Then("the payment doesn't work")
+    public void thePaymentDoesnTWork() {
+        assertTrue(catchedExeption instanceof NotEnoughBalanceException);
+        ArrayList<Payment> payments=new ArrayList<Payment>();
+        paymentRepository.findAllByCustomer(customer).forEach(pay->payments.add(pay));
+        assertEquals(0,payments.size());
     }
 }
--- backend/src/test/resources/features/payment/Payment.feature
+++ backend/src/test/resources/features/payment/Payment.feature
@@ -3,8 +3,28 @@ Background:
   Given a user
   And a store
 
-Scenario: A user wants to pay with his fidelityCard
-  When he want to buy items
+Scenario: A user wants to pay with his fidelityCard and his points
+  When he want to buy an item names "Coffee", it cost 5, in quantity 8
+  When he want to buy an discount names "croissant", it cost 8 and 25 points, in quantity 2
   And he has enough cash
   And he pay
   Then the payment works
+
+Scenario: A user wants to pay with his fidelityCard
+  When he want to buy an item names "Coffee", it cost 5, in quantity 12
+  And he has enough cash
+  And he pay
+  Then the payment works
+
+Scenario: A user wants to pay with his points
+  When he want to buy an discount names "croissant", it cost 8 and 25 points, in quantity 2
+  And he has enough cash
+  And he pay
+  Then the payment works
+
+
+Scenario: A user wants to pay with his fidelityCard but he hasn't enought money
+  When he want to buy an item names "Coffee", it cost 5, in quantity 12
+  And he has not enough cash
+  And he pay
+  Then the payment doesn't work
