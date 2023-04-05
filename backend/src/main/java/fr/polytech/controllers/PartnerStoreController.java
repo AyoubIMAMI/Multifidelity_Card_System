41485692b3ce79cb796df2fa428834e375680c6b
Ayoub <youbima17@gmail.com>
2023-04-05 17:07:58 +0200

    Statistics on StorePartenaireController #23

--- backend/src/main/java/fr/polytech/controllers/PartnerStoreController.java
+++ backend/src/main/java/fr/polytech/controllers/PartnerStoreController.java
@@ -4,6 +4,7 @@ import fr.polytech.controllers.dto.StoreDTO;
 import fr.polytech.entities.Store;
 import fr.polytech.entities.item.Discount;
 import fr.polytech.exceptions.BadCredentialsException;
+import fr.polytech.exceptions.IllegalDateException;
 import fr.polytech.exceptions.discount.DiscountNotFoundException;
 import fr.polytech.exceptions.store.MissingInformationsException;
 import fr.polytech.exceptions.store.StoreSiretAlreadyUsedException;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -48,6 +50,21 @@ public class PartnerStoreController {
     @GetMapping(path = "/statistics", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Double> getTotalCostFromBeginning() {
         return ResponseEntity.ok().body(statsExplorer.getOperationCost());
+    }
+
+    @PostMapping(path = "/statistics", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Double> getTotalCostFromDate(@RequestBody Date date) throws IllegalDateException {
+        return ResponseEntity.ok().body(statsExplorer.getOperationCost(date));
+    }
+
+    @GetMapping(path = "/statistics", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Integer> getTotalPointsUsedFromBeginning() {
+        return ResponseEntity.ok().body(statsExplorer.getTotalPointUsed());
+    }
+
+    @PostMapping(path = "/statistics", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Integer> getTotalPointUsedFromDate(@RequestBody Date date) throws IllegalDateException {
+        return ResponseEntity.ok().body(statsExplorer.getTotalPointUsed(date));
     }
 
     private StoreDTO convertStoreToDto(Store store) {
