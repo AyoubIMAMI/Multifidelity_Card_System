71f9b10bcd3c61d229394f1056abf29bbbf78f69
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-04-06 02:39:51 +0200

    try test script cli

--- DevopsCli/endToEnd.py
+++ DevopsCli/endToEnd.py
@@ -20,6 +20,10 @@ def printTable(table):
     for row in table:
         print(row)
 
+def createTable(input):
+    for row in table:
+        print(row)
+
 def connect():
     global connection
     global cursor
@@ -50,15 +54,34 @@ output = subprocess.check_output(command, shell=True)
 print("out= ",output.decode())
 time.sleep(8)
 
+def assertEquals(expected, actual):
+    if(expected != actual):
+        print(str(expected)+" not equals to "+str(actual))
+        exit(-1)
 
-try:
-    userTable = getTable("customer")
-    printTable(userTable)
-    organisationTable = getTable("organisation")
-    printTable(organisationTable)
-except:
-  print("An exception occurred")
+customerTable_expected = [(1, 'benoit@gmail.com', 0.0, False, None, 30, 'Benoit', 'benocub'),(1, 'benoit@gmail.com', 0.0, False, None, 30, 'Benoit', 'benocub')]
+printTable(customerTable_expected)
 
+organisationTable_expected = [('Store', 3, 'Fast Market', 'yTrEA', '123456789'),('Store', 4, "Vente'2000", 'AzErTy', '987654321')]
+printTable(organisationTable_expected)
+
+paymentTable_expected = [(5, 30.0, True, 1, 4),(6, 25.0, True, 2, 3),(7, 2.0, True, 2, 3)]
+printTable(paymentTable_expected)
+
+
+customerTable = getTable("customer")
+printTable(customerTable)
+
+organisationTable = getTable("organisation")
+printTable(organisationTable)
+
+paymentTable = getTable("payment")
+printTable(paymentTable)
+
+
+assertEquals(customerTable_expected, customerTable)
+assertEquals(organisationTable_expected, organisationTable)
+assertEquals(paymentTable_expected, paymentTable)
 
 # Fermer le curseur et la connexion à la base de données
 cursor.close()
