e857e0e942e3de46c255b94f640039b0f9307d33
Ayoub <youbima17@gmail.com>
2023-04-09 00:43:49 +0200

    documentation PointPurchase

--- backend/src/main/java/fr/polytech/interfaces/payment/PointPurchase.java
+++ backend/src/main/java/fr/polytech/interfaces/payment/PointPurchase.java
@@ -8,5 +8,14 @@ import fr.polytech.entities.Customer;
 import java.util.Set;
 
 public interface PointPurchase {
+
+    /**
+     * Perform a purchase with points
+     * @param customer involved
+     * @param shoppingList items to purchase
+     * @return the involved customer
+     * @throws NotEnoughBalanceException threw if the balance is low
+     * @throws NoDiscountsFoundException threw if the discounts are not been found
+     */
     Customer buyWithPoint(Customer customer, Set<Item> shoppingList) throws NotEnoughBalanceException, NoDiscountsFoundException;
 }
