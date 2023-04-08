ed98f7058bf29320434aab50bdc0574be6126d2c
Ayoub <youbima17@gmail.com>
2023-04-09 00:04:08 +0200

    documentation DiscountModifier

--- backend/src/main/java/fr/polytech/interfaces/discount/DiscountModifier.java
+++ backend/src/main/java/fr/polytech/interfaces/discount/DiscountModifier.java
@@ -8,9 +8,31 @@ import fr.polytech.exceptions.store.StoreNotFoundException;
 
 public interface DiscountModifier {
 
+    /**
+     * Create a discount
+     * @param name discount name
+     * @param storeId store id
+     * @param pointPrice points which can be earned
+     * @return the Discount
+     * @throws NegativeAmountException threw if the amount is negative
+     * @throws StoreNotFoundException threw is the store has not been found
+     */
     Discount createDiscount(String name, Long storeId, int pointPrice) throws NegativeAmountException, StoreNotFoundException;
 
+    /**
+     * Modify the discounts points price
+     * @param id the discount id
+     * @param newPointPrice new amount of points
+     * @return the updated Discount
+     * @throws DiscountNotFoundException threw if the discount has not been found
+     */
     Discount modifyPointPrice(Long id, int newPointPrice) throws DiscountNotFoundException;
 
+    /**
+     * Delete a discount
+     * @param id the discount id
+     * @return a boolean according to "does the discount exist?" (false means "has been deleted")
+     * @throws DiscountNotFoundException threw if the discount has not been found
+     */
     boolean deleteDiscount(Long id) throws DiscountNotFoundException;
 }
