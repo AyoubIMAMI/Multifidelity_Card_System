655468e5b949139c63075027c9770f8f9b1b7897
MouradKarrakchou <91179433+MouradKarrakchou@users.noreply.github.com>
2023-04-08 18:19:22 +0200

    Fixing Discount to use Store #36

--- backend/src/main/java/fr/polytech/interfaces/store/StoreExplorer.java
+++ backend/src/main/java/fr/polytech/interfaces/store/StoreExplorer.java
@@ -3,7 +3,6 @@ package fr.polytech.interfaces.store;
 import fr.polytech.entities.Store;
 import fr.polytech.entities.item.Discount;
 import fr.polytech.exceptions.store.StoreNotFoundException;
-import fr.polytech.entities.Schedule;
 
 import java.util.Set;
 
