46deb129438e1f6d833489b89858ab80f1d55167
Ayoub <youbima17@gmail.com>
2023-04-08 23:30:46 +0200

    documentation VFPAdvantageModifier

--- backend/src/main/java/fr/polytech/interfaces/advantage/VFPAdvantageModifier.java
+++ backend/src/main/java/fr/polytech/interfaces/advantage/VFPAdvantageModifier.java
@@ -4,5 +4,11 @@ import fr.polytech.entities.Customer;
 import fr.polytech.entities.VFPAccount;
 
 public interface VFPAdvantageModifier {
+
+    /**
+     * Add a customer to the VFP program
+     * @param consumer customer to ba added
+     * @return the customer VFP account
+     */
     VFPAccount addCustomerToProgramVFP(Customer consumer);
 }
