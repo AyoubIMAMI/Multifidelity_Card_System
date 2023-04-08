d5127a46b1d1bced5cbf518ef3aa28c44156ffd8
MouradKarrakchou <91179433+MouradKarrakchou@users.noreply.github.com>
2023-04-08 23:00:19 +0200

    fix

--- iSawWhereYouParkedLastSummer/src/main.py
+++ iSawWhereYouParkedLastSummer/src/main.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 def claim():
     data = request.get_json()
 
-    if 'licensePlate' in data and 'parkingId' in data:
+    if 'licensePlate' in data and 'parkingID' in data:
         print('OK for parking')
         return Response(status=201)
-    
+
     else:
         print('NOK for parking, license plate or parking id is missing')
-        error_message = {'error': 'license plate or parking id is missing'}
+        error_message = {'error': 'license plate or parking id is missing', 'request': str(request), 'data': data}
         return Response(json.dumps(error_message), status=400, mimetype='application/json')
 
 if __name__ == '__main__':
