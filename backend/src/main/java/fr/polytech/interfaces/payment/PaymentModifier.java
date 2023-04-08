21dfa0c9a377ae5428dcf5144fcd2c8d7ae10af5
Ayoub <youbima17@gmail.com>
2023-04-09 00:41:46 +0200

    documentation PaymentExplorer

--- backend/src/main/java/fr/polytech/interfaces/payment/PaymentModifier.java
+++ backend/src/main/java/fr/polytech/interfaces/payment/PaymentModifier.java
@@ -4,5 +4,12 @@ import fr.polytech.exceptions.payment.PaymentAlreadyExistsException;
 import fr.polytech.entities.Payment;
 
 public interface PaymentModifier {
+
+    /**
+     * Save a Payment
+     * @param payment to ave
+     * @return the saved Payment
+     * @throws PaymentAlreadyExistsException threw if the Payment already exist
+     */
     Payment savePayment(Payment payment) throws PaymentAlreadyExistsException;
 }
