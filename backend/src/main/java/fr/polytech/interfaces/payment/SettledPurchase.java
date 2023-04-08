64d5ecf5372c692b201d38eea42c98e6ede33f11
Ayoub <youbima17@gmail.com>
2023-04-09 00:48:57 +0200

    documentation SettledPurchase

--- backend/src/main/java/fr/polytech/interfaces/payment/SettledPurchase.java
+++ backend/src/main/java/fr/polytech/interfaces/payment/SettledPurchase.java
@@ -4,6 +4,18 @@ import fr.polytech.entities.Payment;
 import fr.polytech.exceptions.payment.PaymentAlreadyExistsException;
 
 public interface SettledPurchase {
+
+    /**
+     * Validate a purchase
+     * @param payment to validate
+     * @return the Payment
+     * @throws PaymentAlreadyExistsException threw if the Payment already exist
+     */
     Payment validatePurchase(Payment payment) throws PaymentAlreadyExistsException;
+
+    /**
+     * Earn points
+     * @param payment Payment from which the points are earned
+     */
     void winPoint(Payment payment);
 }
