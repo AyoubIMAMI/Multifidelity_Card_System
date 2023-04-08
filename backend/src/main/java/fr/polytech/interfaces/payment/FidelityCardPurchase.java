2d750bf2b577bb4fc39ffb33a1540c36ad1ee45b
Ayoub <youbima17@gmail.com>
2023-04-09 00:32:54 +0200

    documentation FidelityCardPurchase

--- backend/src/main/java/fr/polytech/interfaces/payment/FidelityCardPurchase.java
+++ backend/src/main/java/fr/polytech/interfaces/payment/FidelityCardPurchase.java
@@ -1,6 +1,5 @@
 package fr.polytech.interfaces.payment;
 
-import fr.polytech.entities.Store;
 import fr.polytech.entities.item.Item;
 import fr.polytech.exceptions.NotEnoughBalanceException;
 import fr.polytech.entities.Customer;
@@ -8,5 +7,13 @@ import fr.polytech.entities.Customer;
 import java.util.Set;
 
 public interface FidelityCardPurchase {
+
+    /**
+     * Use the fidelity card to perform a purchase
+     * @param customer involved in the purchase
+     * @param shoppingList items to purchase
+     * @return involved customer
+     * @throws NotEnoughBalanceException threw if the balance is not enough to perform the purchase
+     */
     Customer buyWithFidelityCard(Customer customer, Set<Item> shoppingList) throws NotEnoughBalanceException;
 }
