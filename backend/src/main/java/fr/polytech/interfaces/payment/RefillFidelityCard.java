b37a26c272b53267782e90726a07908c0665b41b
Ayoub <youbima17@gmail.com>
2023-04-09 00:46:21 +0200

    documentation PointPurchase

--- backend/src/main/java/fr/polytech/interfaces/payment/RefillFidelityCard.java
+++ backend/src/main/java/fr/polytech/interfaces/payment/RefillFidelityCard.java
@@ -1,6 +1,5 @@
 package fr.polytech.interfaces.payment;
 
-import fr.polytech.connectors.externaldto.BankTransactionDTO;
 import fr.polytech.entities.Customer;
 import fr.polytech.exceptions.payment.NegativeAmountException;
 import fr.polytech.exceptions.payment.PaymentInBankException;
@@ -8,5 +7,15 @@ import fr.polytech.exceptions.payment.PaymentInBankException;
 import java.util.Date;
 
 public interface RefillFidelityCard {
+
+    /**
+     * Refill the fidelity card
+     * @param customer involved
+     * @param creditCard customer credit card
+     * @param amount to add
+     * @return the Date of the transaction
+     * @throws NegativeAmountException threw if the amount is negative
+     * @throws PaymentInBankException threw if Payment in Bank has been refused
+     */
     Date refill(Customer customer, String creditCard, double amount ) throws NegativeAmountException, PaymentInBankException;
 }
