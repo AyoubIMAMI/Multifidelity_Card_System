e1fb8547e2de1477d9fb79950c9b53a54fbe139e
Ayoub <youbima17@gmail.com>
2023-04-09 00:31:14 +0200

    documentation Bank

--- backend/src/main/java/fr/polytech/interfaces/payment/Bank.java
+++ backend/src/main/java/fr/polytech/interfaces/payment/Bank.java
@@ -1,8 +1,15 @@
 package fr.polytech.interfaces.payment;
 
 import fr.polytech.exceptions.payment.PaymentInBankException;
-import fr.polytech.connectors.externaldto.BankTransactionDTO;
 
 public interface Bank {
+
+    /**
+     * Refill the balance from the Bank
+     * @param creditCard used to refill the customer balance
+     * @param amount to refill
+     * @return true if the Bank accept the transaction/payment
+     * @throws PaymentInBankException threw if the transaction/payment has been refused
+     */
     boolean refill(String creditCard,double amount) throws PaymentInBankException;
 }
