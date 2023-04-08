4f778758d6b4f9d7e843a4300349fba111506377
Ayoub <youbima17@gmail.com>
2023-04-09 00:14:19 +0200

    documentation Parking

--- backend/src/main/java/fr/polytech/interfaces/Parking.java
+++ backend/src/main/java/fr/polytech/interfaces/Parking.java
@@ -3,5 +3,11 @@ package fr.polytech.interfaces;
 import fr.polytech.connectors.externaldto.ParkingTransactionDTO;
 
 public interface Parking {
+
+    /**
+     * Get a parking place
+     * @param parkingTransactionDTO dto
+     * @return a boolean according to the availability of the places
+     */
     boolean getParkingPlace(ParkingTransactionDTO parkingTransactionDTO);
 }
