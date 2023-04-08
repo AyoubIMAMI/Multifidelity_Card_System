7d09ae57950dc995cd68ce597863aac18cf613a5
Ayoub <youbima17@gmail.com>
2023-04-09 00:12:07 +0200

    documentation PointModifier

--- backend/src/main/java/fr/polytech/interfaces/fidelity/PointModifier.java
+++ backend/src/main/java/fr/polytech/interfaces/fidelity/PointModifier.java
@@ -3,7 +3,20 @@ package fr.polytech.interfaces.fidelity;
 import fr.polytech.entities.Customer;
 
 public interface PointModifier {
-    //points computed from the price
+
+    /**
+     * Increment the points which are computed from the price
+     * @param customer involved in the incrementation
+     * @param price of the bought article or service
+     * @return the customer involved
+     */
     Customer incrementPoints(Customer customer, float price);
+
+    /**
+     * Decrement points when they are used to buy something
+     * @param customer involved in the decremental
+     * @param points number of points used
+     * @return the customer involved
+     */
     Customer decrementPoints(Customer customer, int points);
 }
