3e54e2e59d952aca537735301dbe1883a2dd7a4a
Ayoub <youbima17@gmail.com>
2023-04-09 00:41:05 +0200

    documentation PaymentExplorer

--- backend/src/main/java/fr/polytech/interfaces/payment/PaymentExplorer.java
+++ backend/src/main/java/fr/polytech/interfaces/payment/PaymentExplorer.java
@@ -2,22 +2,29 @@ package fr.polytech.interfaces.payment;
 
 import fr.polytech.entities.Customer;
 import fr.polytech.entities.Payment;
-import fr.polytech.exceptions.CustomerNotFoundException;
-import fr.polytech.exceptions.store.StoreNotFoundException;
-import fr.polytech.exceptions.payment.PaymentNotFoundException;
-
-import fr.polytech.entities.Store;
-
 import java.util.Date;
 import java.util.List;
 
 
 public interface PaymentExplorer {
 
-
+    /**
+     * Verify if the customer reached 10 Payments
+     * @param customer involved
+     * @return true if the customer reached 10 Payments
+     */
     boolean customerReached10Payments(Customer customer);
 
+    /**
+     * Find all Payments
+     * @return Payments list
+     */
     List<Payment> findAllPayments();
 
+    /**
+     * Find all the transactions since a Date
+     * @param date after which the transactions are found
+     * @return Payments list
+     */
     List<Payment> findAllByTransactionDateAfter(Date date);
 }
