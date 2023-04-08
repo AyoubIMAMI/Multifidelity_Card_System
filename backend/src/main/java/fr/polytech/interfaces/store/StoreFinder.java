54d9b1454e3837766d36a151e2614489e0934694
Ayoub <youbima17@gmail.com>
2023-04-09 00:17:55 +0200

    documentation StoreFinder

--- backend/src/main/java/fr/polytech/interfaces/store/StoreFinder.java
+++ backend/src/main/java/fr/polytech/interfaces/store/StoreFinder.java
@@ -5,6 +5,21 @@ import fr.polytech.exceptions.BadCredentialsException;
 import fr.polytech.exceptions.store.StoreNotFoundException;
 
 public interface StoreFinder {
+
+    /**
+     * Find a store by credentials
+     * @param storeName store name
+     * @param myPassword store password
+     * @return the Store
+     * @throws BadCredentialsException threw if the credentials does not match
+     */
     Store findStore(String storeName, String myPassword)  throws BadCredentialsException;
+
+    /**
+     * Find a store by ID
+     * @param storeID store ID
+     * @return the Store
+     * @throws StoreNotFoundException threw if the Store has not been found
+     */
     Store findStoreByID(Long storeID) throws StoreNotFoundException;
 }
