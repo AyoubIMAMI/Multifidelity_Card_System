df190408b44619b0a0b6ab6894523d643fd2bde3
Ayoub <youbima17@gmail.com>
2023-04-09 00:19:25 +0200

    documentation StoreRegistration

--- backend/src/main/java/fr/polytech/interfaces/store/StoreRegistration.java
+++ backend/src/main/java/fr/polytech/interfaces/store/StoreRegistration.java
@@ -4,5 +4,14 @@ import fr.polytech.entities.Store;
 import fr.polytech.exceptions.store.StoreSiretAlreadyUsedException;
 
 public interface StoreRegistration {
+
+    /**
+     * Registration of a new store
+     * @param storeName store name
+     * @param storeSiret store siret number
+     * @param password store password
+     * @return the new registered Store
+     * @throws StoreSiretAlreadyUsedException threw if the siret number has already been used
+     */
     Store registerNewStore(String storeName, String storeSiret, String password) throws StoreSiretAlreadyUsedException;
 }
