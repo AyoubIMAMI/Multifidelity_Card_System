7b63b1218ab7bf60421ab1878919871eca9177fb
Ayoub <youbima17@gmail.com>
2023-04-08 23:24:47 +0200

    documentation AdvantageModifier

--- backend/src/main/java/fr/polytech/interfaces/advantage/AdvantageModifier.java
+++ backend/src/main/java/fr/polytech/interfaces/advantage/AdvantageModifier.java
@@ -4,6 +4,18 @@ import fr.polytech.exceptions.advantage.AdvantageNotFoundException;
 import fr.polytech.entities.Advantage;
 
 public interface AdvantageModifier {
+
+    /**
+     * Create an advantage
+     * @param advantageName advantage name to create
+     * @return the created advantage
+     */
     Advantage createAdvantage(String advantageName);
+
+    /**
+     * Delete an advantage
+     * @param advantageID advantage ID
+     * @throws AdvantageNotFoundException threw if the advantage is not found
+     */
     void deleteAdvantage(Long advantageID) throws AdvantageNotFoundException;
 }
