245dfc00612bd1ea0a158411d1833029f37f2ebb
Ayoub <youbima17@gmail.com>
2023-04-08 23:29:21 +0200

    documentation VFPAdvantageModifier

--- backend/src/main/java/fr/polytech/interfaces/advantage/VFPAdvantageFinder.java
+++ backend/src/main/java/fr/polytech/interfaces/advantage/VFPAdvantageFinder.java
@@ -10,7 +10,22 @@ import fr.polytech.exceptions.advantage.VFPNotFoundException;
 import java.util.Optional;
 
 public interface VFPAdvantageFinder {
+
+    /**
+     * Get the customer advantage account
+     * @param customer we want to get the advantage account
+     * @return an Optional of account
+     */
     Optional<VFPAccount> findCustomerAdvantageAccount(Customer customer);
+
+    /**
+     * Verify is the advantage date and use it
+     * @param consumerAdvantageID the VFP account id
+     * @param advantage the advantage to consume
+     * @throws NoAdvantageFoundException threw if the advantage is not found
+     * @throws VFPNotFoundException threw if the VFP account is not found
+     * @throws AdvantageAlreadyConsumedException threw if the advantage has already been consumed
+     */
     void consumeAdvantage(VFPAccount consumerAdvantageID, Advantage advantage) throws NoAdvantageFoundException, VFPNotFoundException, AdvantageAlreadyConsumedException;
 
 }
