f3eda2e80d8e2d4cb3b59c665c6b6ecc9cea7fce
Ayoub <youbima17@gmail.com>
2023-04-08 23:22:14 +0200

    documentation AdvantageExplorer

--- backend/src/main/java/fr/polytech/interfaces/advantage/AdvantageExplorer.java
+++ backend/src/main/java/fr/polytech/interfaces/advantage/AdvantageExplorer.java
@@ -10,7 +10,18 @@ import java.util.Optional;
 
 public interface AdvantageExplorer {
 
+    /**
+     * Used to verify if an advantage exists from the advantage repository
+     * @param advantageID the advantage ID
+     * @return an Optional Advantage, since the advantage can expire
+     */
     Optional<Advantage> VerifyAdvantage(Long advantageID);
 
+    /**
+     * Get the advantage but throw if it is not found
+     * @param advantageId the advantage ID
+     * @return the Advantage
+     * @throws AdvantageNotFoundException threw if not found
+     */
     Advantage findAdvantageById(Long advantageId) throws AdvantageNotFoundException;
 }
