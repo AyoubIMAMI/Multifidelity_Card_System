9668039ebfcabef9713cf0b0d773d7cb9a69e852
Ayoub <youbima17@gmail.com>
2023-04-08 23:37:12 +0200

    documentation CustomerRegistration

--- backend/src/main/java/fr/polytech/interfaces/customer/CustomerRegistration.java
+++ backend/src/main/java/fr/polytech/interfaces/customer/CustomerRegistration.java
@@ -5,7 +5,23 @@ import fr.polytech.exceptions.MailAlreadyUsedException;
 import fr.polytech.entities.Customer;
 
 public interface CustomerRegistration {
+
+    /**
+     * Register a new user
+     * @param name user's name
+     * @param mail user's mail
+     * @param password user's password
+     * @return the new Customer registered
+     * @throws MailAlreadyUsedException threw if the mail has already been used
+     */
     Customer register(String name, String mail, String password) throws MailAlreadyUsedException;
 
+    /**
+     * Register a car license plate
+     * @param customerID customer ID
+     * @param licensePlate car license plate
+     * @return the customer involved int this registration
+     * @throws CustomerNotFoundException threw if the customer is not found
+     */
     Customer registerNewPlate(Long customerID,String licensePlate) throws CustomerNotFoundException;
 }
