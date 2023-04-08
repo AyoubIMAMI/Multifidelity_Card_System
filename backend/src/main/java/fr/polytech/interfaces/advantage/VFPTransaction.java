dbb768df4c4313187f345c8ece54e5c548948762
Ayoub <youbima17@gmail.com>
2023-04-08 23:34:45 +0200

    documentation VFPTransaction

--- backend/src/main/java/fr/polytech/interfaces/advantage/VFPTransaction.java
+++ backend/src/main/java/fr/polytech/interfaces/advantage/VFPTransaction.java
@@ -7,7 +7,28 @@ import fr.polytech.exceptions.advantage.NoAdvantageFoundException;
 import fr.polytech.exceptions.advantage.VFPNotFoundException;
 
 public interface VFPTransaction {
+
+    /**
+     * Do all the verification on an advantage before using it
+     * @param userID user ID who wants to use an advantage
+     * @param advantageID advantage ID to be used
+     * @throws CustomerNotFoundException threw if the customer is not found
+     * @throws NoAdvantageFoundException threw if the advantage is not found
+     * @throws VFPNotFoundException if the VFP account is not found
+     * @throws AdvantageAlreadyConsumedException threw is the advantage has already been used
+     */
     void tryUseAdvantage(Long userID, Long advantageID) throws CustomerNotFoundException, NoAdvantageFoundException, VFPNotFoundException, AdvantageAlreadyConsumedException;
 
+    /**
+     * Do all the verification through the tryUseAdvantage method and add a verification on the parking service
+     * @param userID customer ID
+     * @param advantageID advantage ID
+     * @param parkingID parking ID
+     * @throws CustomerNotFoundException threw if the customer is not found
+     * @throws NoAdvantageFoundException threw if the advantage is not found
+     * @throws VFPNotFoundException if the VFP account is not found
+     * @throws AdvantageAlreadyConsumedException threw is the advantage has already been used
+     * @throws ParkingUnavailableException threw is the parking is unavailable
+     */
     void tryUseParkingAdvantage(Long userID, Long advantageID, Long parkingID) throws CustomerNotFoundException, NoAdvantageFoundException, VFPNotFoundException, AdvantageAlreadyConsumedException, ParkingUnavailableException;
 }
