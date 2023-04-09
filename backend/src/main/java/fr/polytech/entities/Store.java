cf274a6b22f19f885a1341a06096fb432f3d9139
Antoine Buquet <61704871+antoinebqt@users.noreply.github.com>
2023-04-09 10:44:49 +0200

    fix payment cli

--- backend/src/main/java/fr/polytech/entities/Store.java
+++ backend/src/main/java/fr/polytech/entities/Store.java
@@ -19,27 +19,9 @@ public class Store extends Organisation {
 
     public Store(String storeName, String storeSiret, String password) {
         super(storeSiret, storeName, password);
-        payments = new HashSet<>();
-        offers = new HashSet<>();
     }
 
     public Store() {
 
-    }
-
-    public Set<Payment> getPayments() {
-        return payments;
-    }
-
-    public void setPayments(Set<Payment> payments) {
-        this.payments = payments;
-    }
-
-    public Set<Discount> getOffers() {
-        return offers;
-    }
-
-    public void setOffers(Set<Discount> offers) {
-        this.offers = offers;
     }
 }
